2427ba3389c017f9972f8f4fecd7f80b0e5ce15f
Arifur999 <arr96777@gmail.com>
2025-11-17 20:10:22 +0600

    problem 4

--- solution.ts
+++ solution.ts
@@ -13,6 +13,8 @@ function formatValue(value: string | number | boolean): string | number | boolea
 }
 
 
+
+
 function getLength(value: string | any[]): number {
   if (typeof value === "string") {
     return value.length;
@@ -41,13 +43,39 @@ class Person {
   }
 
   getDetails(): string {
-    return `Name: ${this.name}, Age: ${this.age}`;
+    return `'Name: ${this.name}, Age: ${this.age}'`;
   }
 }
 
-
 const person1 = new Person('John Doe', 30);
-console.log(person1.getDetails());  
+console.log(person1.getDetails());
 
 const person2 = new Person('Alice', 25);
 console.log(person2.getDetails());
+
+
+
+
+
+
+
+interface Item {
+  title: string;
+  rating: number; 
+}
+
+function filterByRating(items: Item[]): Item[] {
+  return items.filter(item => item.rating >= 4);
+}
+
+
+const books = [
+  { title: 'Book A', rating: 4.5 },
+  { title: 'Book B', rating: 3.2 },
+  { title: 'Book C', rating: 5.0 },
+];
+
+console.log(filterByRating(books));
+
+
+
